040c13ad9f250f685ecdbc0df0a56ab161229179
azbayor2 <hanharry2@gmail.com>
2026-03-09 21:22:48 +0900

    feat: 파이썬 더미 백엔드 구축

---
+++ python/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def hello_world():
+    return jsonify(data="hello world")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8888)
